ad139d027843e478fd172b94405e35945d2f4c1f
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-05-03 01:22:58 -0400

    add line

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
         return (
             <div>
                 <TitleBar />
+                <Hello>
                 <MainPage />
             </div>
     );
